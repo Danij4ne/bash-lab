96b8aebee5a549931f6323b6e34bbbbeca389b06
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:29:56 +0100

    update

---
+++ 01file_management/00default_directories.bash
@@ -0,0 +1,28 @@
+
+# Default UNIX directories
+
+/bin   # basic programs
+
+/home  # personal user folders
+
+/etc   # configuration files
+
+/opt   # install complete external programs
+
+/var   # contains variable system data such as logs, queues, databases, caches
+
+/usr   # additional software
+
+/tmp   # temporary files that delete themselves
+
+
+
+
+
+
+
+
+
+
+
+
