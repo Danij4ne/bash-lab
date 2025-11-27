9b56f535e742b88504de640bd1f74a2e760d33e2
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:29:45 +0100

    update

---
+++ 00first_steps/05command_anatomy.bash
@@ -0,0 +1,16 @@
+
+# Structure
+
+# command [options] [arguments]
+
+# - The command is the main action to be executed.
+# - Options modify the behavior of the command (usually start with - or --).
+# - Arguments specify the target the command acts on (files, paths, etc.).
+
+
+
+
+
+
+
+
