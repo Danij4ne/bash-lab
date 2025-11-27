895f536471485913887c58031bcaabbb4863550c
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:30:11 +0100

    update

---
+++ 01file_management/03advanced_listings.bash
@@ -0,0 +1,37 @@
+
+# tree
+
+tree          # shows a tree of directories and files
+
+tree -a       # -a = hidden files, shows all files and directories including hidden ones
+
+find          # used to find files by name or by criteria
+
+find . -name "README.md"   # search by -name (name) README.md
+
+find . -name "READ*"       # everything that starts with READ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
