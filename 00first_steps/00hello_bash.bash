fa4b683f4ac18a2531fecb0dbc83f160fd77a002
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:28:14 +0100

    update

---
+++ 00first_steps/00hello_bash.bash
@@ -0,0 +1,8 @@
+echo "Hello Bash"  #(echo prints what you write)
+
+echo $SHELL       #(the way to see which shell we are using)
+
+echo $0           #(gives us the name of the Bash shell, also shows what we are working with)
+
+
+
