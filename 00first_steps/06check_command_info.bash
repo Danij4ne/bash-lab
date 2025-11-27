54a366a5fefc683a250e8cbee4c0162b87f93ca8
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:29:50 +0100

    updte

---
+++ 00first_steps/06check_command_info.bash
@@ -0,0 +1,18 @@
+# Word man = manual
+
+ls man        # consult all functionalities of ls
+
+whoami man    # consult all functionalities of whoami
+
+
+# --help = not available for every command
+
+python3 --help   # shows how to use it, similar to man
+
+
+# -h = abbreviation of help, shows how to use the command
+
+python3 -h
+
+
+
