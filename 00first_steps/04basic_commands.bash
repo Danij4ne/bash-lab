41e328df439f5dfc9f71eb6b42317267da27f25d
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:29:21 +0100

    Add basic system command examples script

---
+++ 00first_steps/04basic_commands.bash
@@ -0,0 +1,32 @@
+
+whoami      # who am I (shows the username of the system)
+
+cal         # shows the system calendar
+
+date        # current date and time
+
+uptime      # how long the machine has been running
+
+hostname    # name of the computer
+
+uname       # name of the system we are using
+
+uname -a    # gives more detailed system information
+
+clear       # clears the screen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
