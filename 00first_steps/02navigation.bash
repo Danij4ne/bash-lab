6a8e1dff88a246676e29ab199883443c2e7c58e5
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:28:23 +0100

    update

---
+++ 00first_steps/02navigation.bash
@@ -0,0 +1,24 @@
+
+cd                # used to change directory
+
+cd Desktop        # enter Desktop
+
+cd ..             # go one directory back
+
+cd ../../../../   # each set of two dots moves you one folder back
+
+cd ~              # takes you directly to your home directory of the current user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
