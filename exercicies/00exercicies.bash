7fd6412b3771e7dd38d2e6735115782cbaecca90
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:30:55 +0100

    exercices

---
+++ exercicies/00exercicies.bash
@@ -0,0 +1,55 @@
+
+## 00Exercises
+
+#1. In the terminal, show the directory you are currently in.
+
+pwd
+
+#2. Change to the Documents directory of your system.
+
+cd Documents
+
+#3. Return to the same directory (Documents), but this time using a full absolute path.
+
+cd /Users/Danij4ne/Documents
+
+#4. Go up one level in the directory hierarchy.
+
+cd ..
+
+#5. List the contents of the current directory in simple format, then long format, and finally including hidden files.
+
+ls 
+
+ls -l
+
+ls -a
+
+#6. Check the manual of any command.
+
+man ls
+
+#7. Check the help of any command.
+
+python3 --help
+
+#8. Display your username, the current date and time, and this month's calendar.
+
+whoami
+
+date
+
+cal
+
+#9. Return to the directory where you started in the first exercise.
+
+cd ~
+
+#11. Clear the screen.
+
+clear
+
+
+
+
+
