8dd2546a03d01eec96fbba0a7c18048e40b1a7e1
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:31:01 +0100

    exercices

---
+++ exercicies/02exercices.bash
@@ -0,0 +1,22 @@
+
+## Exercises
+
+#1. Display all the content of a file.
+
+#2. Display the content of a file paginated.
+
+#3. Display the first 15 lines of a file.
+
+#4. Display the last 15 lines of a file.
+
+#5. Search for a word inside a file.
+
+#6. Count the lines of a file.
+
+#7. Redirect an output and save it into a file.
+
+#8. Append a new output to the previous file.
+
+#9. Chain 3 commands.
+
+#10. Create a local variable and display it.
