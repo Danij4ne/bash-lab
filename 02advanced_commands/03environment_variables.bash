0d6265607837204e24b9c09c572b613207e1dab5
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:30:41 +0100

    update

---
+++ 02advanced_commands/03environment_variables.bash
@@ -0,0 +1,17 @@
+
+name="Dani"    # I create a variable called name (it will only be known in the session where it is created)
+
+$              # used to access variables
+
+echo $name     # it will print Dani
+
+### Plus
+
+.bash_profile  # this hidden file runs automatically when you start a Bash session
+
+export Name="Dani"   # you add this line to permanently save a variable
+
+# in every new session you open, if you call Name it will return Dani
+
+
+
