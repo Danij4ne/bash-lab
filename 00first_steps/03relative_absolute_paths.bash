8ecccd4c5e62cf6ec08217f79cd922d0b42a98a8
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:28:29 +0100

    update

---
+++ 00first_steps/03relative_absolute_paths.bash
@@ -0,0 +1,41 @@
+
+# Absolute path
+cd /Users/dani/Documentos/proyect   # Go to the folder using an absolute path
+
+# Relative path
+cd ../proyect                       # Enter a folder named "proyecto" that is in the current directory
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
