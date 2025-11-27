bbf8354eadfa7a7a055606c0138ce404aa00f52c
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:30:18 +0100

    update

---
+++ 02advanced_commands/00file_reading.bash
@@ -0,0 +1,24 @@
+
+# File reading
+
+cat            # displays the content of the file on screen
+
+less           # for longer files, allows you to paginate the content
+
+more           # similar to less but with fewer features
+
+head           # shows only the first 10 lines of a file
+
+head -n 20     # -n specifies how many lines you want to see, 20 is the number of lines shown
+
+tail           # shows the last 10 lines
+
+tail -n 20     # shows the last 20 lines
+
+tail -f        # even if the file grows in real time, -f will always show the last lines
+
+
+
+
+
+
