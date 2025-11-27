f7652ca0f4f36de5c4fead3cb22f6f12393f9cb7
Daniel Jane <danij4ne@gmail.com>
2025-11-27 09:28:19 +0100

    update

---
+++ 00first_steps/01orientation.bash
@@ -0,0 +1,12 @@
+pwd      # know where you are located
+
+ls       # used to list the files and folders inside the directory you are in
+
+ls -l    # shows complete information about the files, including permissions
+
+ls -a    # shows hidden files
+
+ls -lh   # also shows complete information but in a more abbreviated/human-readable format
+
+ls /     # lists the contents of the root directory of the system, showing all main Linux folders
+
