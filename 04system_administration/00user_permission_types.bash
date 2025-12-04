b21a99ac295c07c8fd4a487a9802e12b292f6e9c
Daniel Jane <danij4ne@gmail.com>
2025-12-04 09:43:55 +0100

    Add user permission types reference script

---
+++ 04system_administration/00user_permission_types.bash
@@ -0,0 +1,18 @@
+
+# Permission types
+
+read        -> r
+write       -> w
+execute     -> x
+
+# User types
+
+owner user  -> u
+group       -> g
+others      -> o
+all         -> a
+
+# View permissions
+
+ls -l file.txt    # shows file permissions, owner, and group
+ls -ld folder     # shows permissions of a directory
