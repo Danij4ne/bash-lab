c3e94ea269ad236890c8b5b36ec37e3db5c3001e
Daniel Jane <danij4ne@gmail.com>
2025-12-04 09:44:25 +0100

    Add bash exercises for nano and vim editors

---
+++ exercises/03exercicies.bash
@@ -0,0 +1,56 @@
+
+# NANO
+# * Create a file named nota.txt and write three lines:
+#   nano nota.txt
+#   (write the lines)
+#   Ctrl + O  -> save
+#   Ctrl + X  -> exit
+
+# * Open nota.txt, add a line at the end, and save:
+#   nano nota.txt
+#   (go to the end, type the new line)
+#   Ctrl + O  -> save
+#   Ctrl + X  -> exit
+
+# * Open a new file recordatorio.txt, write something, and exit WITHOUT saving:
+#   nano recordatorio.txt
+#   (write anything)
+#   Ctrl + X  -> Nano asks to save
+#   Press N   -> exit without saving
+
+# * Search for a specific word inside a file:
+#   nano file.txt
+#   Ctrl + W  -> search
+#   (type the word and press Enter)
+
+# * Cut a line and paste it somewhere else:
+#   Ctrl + K  -> cut line
+#   Move cursor to target location
+#   Ctrl + U  -> paste line
+
+
+# VIM
+# * Create apuntes.txt, enter insert mode, and write a sentence:
+#   vim apuntes.txt
+#   i        -> insert mode
+#   (write the sentence)
+#   Esc      -> exit insert mode
+
+# * Move the cursor without using arrow keys:
+#   h  -> left
+#   j  -> down
+#   k  -> up
+#   l  -> right
+
+# * Delete a full line and undo the change:
+#   dd   -> delete line
+#   u    -> undo
+
+# * Copy a line and paste it below:
+#   yy   -> yank (copy) line
+#   p    -> paste below
+
+# * Save changes and exit:
+#   :wq   -> write + quit
+#   OR
+#   :x    -> save and exit
