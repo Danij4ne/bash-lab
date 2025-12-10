30e4fac8e9282102ca4cddc738d42eae7a87b09c
Daniel Jane <danij4ne@gmail.com>
2025-12-10 08:45:10 +0100

    add notes about sudo

---
+++ 04system_administration/04_sudo_superuser.bash
@@ -0,0 +1,9 @@
+
+# What is sudo?
+# It means "superuser do".
+# It allows you to execute a command with administrator privileges.
+
+
+sudo apt update        # updates the system package list
+sudo apt install git   # installs the git program
+sudo nano /etc/hosts   # opens the config file with root permissions
