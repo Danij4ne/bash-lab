fad8ff90644a24a2ad903f64390707bc1c3ea026
Daniel Jane <danij4ne@gmail.com>
2025-12-11 09:08:05 +0100

    add process management notes to 00processes.bash

---
+++ 05processes_and_aliases/00processes.bash
@@ -0,0 +1,16 @@
+
+ps       # Shows the processes I am running in my current session
+
+ps aux   # Lists all processes running on the operating system
+
+top      # Displays real-time interactive information about processes and system resource usage
+
+htop     # Improved and interactive version of 'top' (if installed)
+
+df -h    # Shows available disk space (human-readable format)
+
+du -sh * # Shows the size of each folder inside the current directory
+
+du -sh   # Shows the total size of the current directory
+
+du       # Displays detailed size information for all directories and files
