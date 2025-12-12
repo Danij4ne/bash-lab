25773e7e408562a046d1ec185e2c30056b72b7ab
Daniel Jane <danij4ne@gmail.com>
2025-12-12 09:09:59 +0100

    Add notes about Bash history usage examples

---
+++ 05processes_and_aliases/02history.bash
@@ -0,0 +1,7 @@
+
+history   # command history that shows the commands we have executed
+
+!10       # runs the command that is in position 10 in the history list
+
+!ls       # executes the most recent command in the history that started with "ls"
+
