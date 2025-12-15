a8f9845e7f1eb9bd93ac513338b78769e7071338
Daniel Jane <danij4ne@gmail.com>
2025-12-15 08:34:25 +0100

    Add notes about example script for alias and unalias usage

---
+++ 05processes_and_aliases/03alias.bash
@@ -0,0 +1,14 @@
+
+ alias # used to create a short name for a long command
+
+ls -l
+# lists files showing detailed information
+
+alias ll='ls -l'
+# creates an alias called ll that runs the ls -l command
+
+ll
+# executes the command associated with the ll alias (ls -l)
+
+unalias ll
+# removes the ll alias
