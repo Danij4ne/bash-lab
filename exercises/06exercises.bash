2e38f72c9fd8aa40b13fbed53c26fd8eeabf06b8
Daniel Jane <danij4ne@gmail.com>
2025-12-22 08:47:27 +0100

    add bash exercises and fix misnamed exercises

---
+++ exercises/06exercises.bash
@@ -0,0 +1,20 @@
+
+#1 - Create a script that prints on the screen: Hello world from Bash.
+
+#2 - Create a script that displays the current date and the current directory.
+
+#3 - Create a script that stores your name in a variable and prints it on the screen.
+
+#4 - Create a script that declares two numeric variables, then adds, subtracts, and multiplies them, displaying the result of each operation.
+
+#5 - Create a script that asks for your name using read and displays it.
+
+#6 - Create a script that asks the user for two numbers and displays their sum.
+
+#7 - Create a script that receives three arguments and displays the first and the third one.
+
+#8 - Create a script that receives arguments and displays the total number of arguments.
+
+#9 - Create a script that receives two numbers as arguments and displays their sum, subtraction, multiplication, and division.
+
+#10 - Create a script that creates a text file and saves your name inside it.
